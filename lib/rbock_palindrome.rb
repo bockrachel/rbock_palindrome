34abccfac83383b7a780c8ffb765fbd8d0c0caa5
Rachel Bock <bockr@aetna.com>
2023-01-23 16:43:21 -0700

    Initial Commit

---
+++ lib/rbock_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rbock_palindrome/version"
+
+module RbockPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rbock_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RbockPalindrome
+  VERSION = "0.1.0"
+end
