2314b3bbb9a1fc63f5c1d9370f34a5d532bb3722
Rachel Bock <bockr@aetna.com>
2023-01-23 17:43:13 -0700

    Finish working and refactored palindrome method

--- lib/rbock_palindrome.rb
+++ lib/rbock_palindrome.rb
@@ -2,7 +2,25 @@
 
 require_relative "rbock_palindrome/version"
 
-module RbockPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+# module RbockPalindrome
+#   class Error < StandardError; end
+#   # Your code goes here...
+
+# end
+
+class String
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # Returns the letters in the string
+  # def letters
+  #   self.chars.select { |c| c.match(/[a-z]/i)}.join
+  # end
+
+  private
+
+    def processed_content
+      self.scan(/[a-z]/i).join.downcase
+    end
 end
